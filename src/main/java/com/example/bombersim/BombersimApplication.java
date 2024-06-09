254e7e7d25a2190299ab6e7ff2c9049f20c99ba1
siimkk <siimkkoger@gmail.com>
2024-06-10 00:21:12 +0300

    Initial commit

---
+++ src/main/java/com/example/bombersim/BombersimApplication.java
@@ -0,0 +1,13 @@
+package com.example.bombersim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombersimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BombersimApplication.class, args);
+    }
+
+}
